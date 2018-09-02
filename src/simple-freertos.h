01bdc61401b6384fd18dae9a2ee9511d3620f1c2
Francisco E. Paez <franpaez+github@gmail.com>
2018-09-02 10:37:24 -0300

    Wrappers para vTaskDelay y vTaskDelayUntil

--- src/simple-freertos.h
+++ src/simple-freertos.h
@@ -1,6 +1,24 @@
 #ifndef simple-freertos_h
 #define simple-freertos_h
 
+/**
+ * Funcion para la tarea
+ */
+#define tarea(nombre) void nombre(void *p)
+
+/**
+ * Crea tarea
+ */
 #define crearTarea(funcion, nombre, prio) xTaskCreate(funcion, nombre, 256, NULL, prio, NULL)
+
+/**
+ * Invoca la función vTaskDelay
+ */
+#define esperar(ms) vTaskDelay( ms / portTICK_PERIOD_MS )
+
+/**
+ * Invoca la función vTaskDelayUntil
+ */
+#define esperarPeriodo(ms) vTaskDelayUntil( ms / portTICK_PERIOD_MS )
 
 #endif
