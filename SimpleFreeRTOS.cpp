4bef38626b558aabe8321679893d5bea78dab1b3
fepz <franpaez@gmail.com>
2018-12-06 10:27:30 -0300

    Cambios en formato de código.

--- SimpleFreeRTOS.cpp
+++ SimpleFreeRTOS.cpp
@@ -4,12 +4,12 @@
 /**
  * Crea tarea
  */
-void creaTarea( void (*task) (void*), UBaseType_t prio, int periodo) {
+void creaTarea(void (*task) (void*), UBaseType_t prio, int periodo) {
   TaskHandle_t t;
   struct task_info *ti;
 
   /* Create a structure for use by this task. */
-  ti = (struct task_info*) pvPortMalloc( sizeof( struct task_info ) );
+  ti = (struct task_info*) pvPortMalloc(sizeof(struct task_info));
 
   /* Set the structure members. */
   ti->awake = 0;
@@ -19,20 +19,20 @@ void creaTarea( void (*task) (void*), UBaseType_t prio, int periodo) {
   xTaskCreate(task, NULL, 128,  NULL,  prio,  &t);
 
   /* Set the TLS */
-  vTaskSetThreadLocalStoragePointer( t, 0, ( void * ) ti );
+  vTaskSetThreadLocalStoragePointer(t, 0, (void *) ti);
 }
 
 /**
  * Espera t ms.
  */
 void esperar(TickType_t ms) {
-  vTaskDelay(ms / portTICK_PERIOD_MS );
+  vTaskDelay(ms / portTICK_PERIOD_MS);
 }
 
 /**
  * Espera hasta el proximo periodo.
  */
 void esperarPeriodo() {
-  struct task_info *ti = ( struct task_info* ) pvTaskGetThreadLocalStoragePointer( NULL, 0 );
-  vTaskDelayUntil(&(ti->awake), ti->period / portTICK_PERIOD_MS );
+  struct task_info *ti = (struct task_info*) pvTaskGetThreadLocalStoragePointer(NULL, 0);
+  vTaskDelayUntil(&(ti->awake), ti->period / portTICK_PERIOD_MS);
 }
